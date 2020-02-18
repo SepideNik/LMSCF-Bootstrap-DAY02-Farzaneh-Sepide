4d613a38d1bc95914f0a736e41b33fa6ee62c518
Sepide <Sepide.niki@gmail.com>
2020-02-18 15:06:41 +0100

    advance

---
+++ Challange/json/hotel.js
@@ -0,0 +1,66 @@
+let Hotels = JSON.parse(`{
+    "hotel": [
+       {
+         "id": "1",
+         "name": "Gran Melia Ghoo Hotel",
+         "City": "Salman Shahr",
+         "image": "img/hotels/GranMelia.jpg",
+         "Decs": "Meliá Hotels International has announced its entry into the Iranian market with the first five-star branded hotel to be managed by an international hotel company, the Gran Melia Ghoo Hotel, located in the largest mixed residential, commercial and hotel complex to be developed in Iran, currently being built in Salman Shahr, a popular holiday destination on the Caspian Sea.",
+         "Address": "The hotel is part of the Ghoo",
+         "star" : 4.1,
+         "price": 140
+
+       },
+       {
+         "id": "2",
+         "name": "Evin Hotel",
+         "City": "Tehran",
+         "image": "img/hotels/evin-hotel.jpg",
+         "Decs": "It is located near the International Trade Fair in Shemiran. Pleasant and unforgettable moments at the lobby with soft piano music. Quite close to the International Exhibition.",
+         "Address": "Shahid Chamran Broadway, after Parkway Crossroad",
+         "star" : 5,
+         "price": 200
+
+       },
+       {
+         "id": "3",
+         "name": "Dizin Hotel",
+         "City": "Tehran",
+         "image": "img/hotels/dizinHotel.jpg",
+         "Decs": "Dizin Hotel Located on the foot of mountains where there are ski resorts. It has a beautiful view of mountains covered by snow. The hotel consists of two buildings, hotel No.1 and No.2. Dizin is the most important ski resort in the Middle East and is located in the city of Karaj in the province of Alborz. Get acquainted with this beautiful track with Let's go Iran..",
+         "Address": "Dizin,Tehran,Iran",
+         "star" : 3.6,
+         "price": 150
+       },
+       {
+         "id": "4",
+         "name": "Malek-o Tojjar Hotel",
+         "City": "Yayd",
+         "image": "img/hotels/tojjarHotel.jpg",
+         "Decs": "The most noteworthy 5-star Malek-o Tojjar Yazd Hotel is an antique mansion. This previously belonged to a businessman for four generations first of all. Its location is in Pnje Ali Bazaar, Ghiam Avenue in Yazd.",
+         "Address": "Panjeh-ali Bazaar, Yazd Iran",
+         "star" : 5,
+         "price": 180
+       },
+       {
+         "id": "5",
+         "name": "Shiraz Hotel",
+         "City": "Shiraz",
+         "image": "img/hotels/ShirazHotel.jpg",
+         "Decs": "Located in the small spa town of Egerszalók, Mesés Shiraz Wellness & Tréning Hotel Superior features an oriental spa. It offers free Wi-Fi, a breakfast buffet and free parkin.",
+         "Address": "Szechenyi utca 31, Egerszalok 3394 Ungarn",
+         "star" : 4.5,
+         "price": 140
+       },
+       {
+         "id": "6",
+         "name": "Narenjestan Hotel",
+         "City": "Noor",
+         "image": "img/hotels/Narenjestan.jpg",
+         "Decs": "Meliá Hotels International has announced its entry into the Iranian market with the first five-star branded hotel to be managed by an international hotel company, the Gran Melia Ghoo Hotel, located in the largest mixed residential, commercial and hotel complex to be developed in Iran, currently being built in Salman Shahr, a popular holiday destination on the Caspian Sea.",
+         "Address": "Izadshahr, Mazandaran Province, Iran",
+         "star" : 4,
+         "price": 90
+       }
+    ]
+ }`)
---
+++ Challange/json/tour.js
@@ -0,0 +1,68 @@
+let tours = JSON.parse(`{
+    "tour": [
+       {
+         "id": "1",
+         "name": "Persepolis",
+         "City": "Shiraz",
+         "image": "img/places/persepolis.jpg",
+         "Decs": "Persepolis was the ceremonial capital of the Achaemenid Empire. It is situated 60 kilometres northeast of the city of Shiraz in Fars Province, Iran. The earliest remains of Persepolis date back to 515 BC. It exemplifies the Achaemenid style of architecture.",
+         "Period": "1 Day",
+         "price": "100",        
+         "date": "03 Mar 2020"
+
+       },
+       {
+         "id": "2",
+         "name": "Nasir al-Mulk Mosque",
+         "City": "Shiraz",
+         "image": "img/places/NasirMulk.jpg",
+         "Decs": "The Nasir al-Mulk Mosque, also known as the Pink Mosque, is a traditional mosque in Shiraz, Iran. It is located in Gawd-i Arabān quarter, near Shāh Chérāgh Mosque. It was built during Qajar dynasty rule of Iran..",
+         "Period": "2 Day",
+         "price": "500",        
+         "date": "10 Mar 2020"
+
+       },
+       {
+        "id": "3",
+         "name": "Persepolis",
+         "City": "Shiraz",
+         "image": "img/places/persepolis.jpg",
+         "Decs": "In 1971, Carolyn and Roger Perron move their family into a dilapidated Rhode Island farm house and soon strange things start happening around it with escalating nightmarish terror. In desperation, Carolyn contacts the noted paranormal investigators, Ed and Lorraine Warren, to examine the house. What the Warrens discover is a whole area steeped in a satanic haunting that is now targeting the Perron family wherever they go. To stop this evil, the Warrens will have to call upon all their skills and spiritual strength to defeat this spectral menace at its source that threatens to destroy everyone involved.",
+         "Period": "1 Day",
+         "price": "100",        
+         "date": "03 Mar 2020"
+       },
+       {
+         "id": "4",
+         "name": "Persepolis",
+         "City": "Shiraz",
+         "image": "img/places/persepolis.jpg",
+         "Decs": "In 1971, Carolyn and Roger Perron move their family into a dilapidated Rhode Island farm house and soon strange things start happening around it with escalating nightmarish terror. In desperation, Carolyn contacts the noted paranormal investigators, Ed and Lorraine Warren, to examine the house. What the Warrens discover is a whole area steeped in a satanic haunting that is now targeting the Perron family wherever they go. To stop this evil, the Warrens will have to call upon all their skills and spiritual strength to defeat this spectral menace at its source that threatens to destroy everyone involved.",
+         "Period": "1 Day",
+         "price": "100",        
+         "date": "03 Mar 2020"
+         "Genre": "Drama, Horror",         
+         "Like": "45"
+       },
+       {
+         "id": "5",
+         "name": "Persepolis",
+         "City": "Shiraz",
+         "image": "img/places/persepolis.jpg",
+         "Decs": "In 1971, Carolyn and Roger Perron move their family into a dilapidated Rhode Island farm house and soon strange things start happening around it with escalating nightmarish terror. In desperation, Carolyn contacts the noted paranormal investigators, Ed and Lorraine Warren, to examine the house. What the Warrens discover is a whole area steeped in a satanic haunting that is now targeting the Perron family wherever they go. To stop this evil, the Warrens will have to call upon all their skills and spiritual strength to defeat this spectral menace at its source that threatens to destroy everyone involved.",
+         "Period": "1 Day",
+         "price": "100",        
+         "date": "03 Mar 2020"
+       },
+       {
+         "id": "6",
+         "name": "Persepolis",
+         "City": "Shiraz",
+         "image": "img/places/persepolis.jpg",
+         "Decs": "In 1971, Carolyn and Roger Perron move their family into a dilapidated Rhode Island farm house and soon strange things start happening around it with escalating nightmarish terror. In desperation, Carolyn contacts the noted paranormal investigators, Ed and Lorraine Warren, to examine the house. What the Warrens discover is a whole area steeped in a satanic haunting that is now targeting the Perron family wherever they go. To stop this evil, the Warrens will have to call upon all their skills and spiritual strength to defeat this spectral menace at its source that threatens to destroy everyone involved.",
+         "Period": "1 Day",
+         "price": "100",        
+         "date": "03 Mar 2020"
+       }
+    ]
+ }`)
